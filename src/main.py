d1bf72d659fbbe149499f76f9df46b9f6e2da671
Alexander J. Slivka <slivka.alex@gmail.com>
2025-02-13 16:36:47 -0500

    Default commit

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
-# src/main.py
+import json
 from typing import NoReturn
 from pathlib import Path
 from xml_generator import XmlGenerator
 
 
+def load_config(config_path: Path) -> dict:
+    with open(config_path, "r") as config_file:
+        return json.load(config_file)
+
+
 def main() -> NoReturn:
     project_dir = Path(__file__).parent.parent
+    config_path = project_dir / "config.json"
+    config = load_config(config_path)
+
     schemas_dir = project_dir / "schemas"
-    xsd_path = schemas_dir / "template.xsd"
+    xsd_path = schemas_dir / config["xsd_file"]
 
     output_dir = project_dir / "output"
-    xml_path = output_dir / "sample_output.xml"
+    xml_path = output_dir / config["xml_file"]
 
     xml_generator = XmlGenerator(xsd_path, xml_path)
     xml_generator.generate_sample_xml()
