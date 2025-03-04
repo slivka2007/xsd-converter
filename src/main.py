629306bfb332b8b41f8963e7d22eaec10e4703e0
Alexander J. Slivka <slivka.alex@gmail.com>
2025-03-04 13:09:12 -0500

    update main

--- src/main.py
+++ src/main.py
@@ -1,14 +1,8 @@
 # src/main.py
-
 from typing import NoReturn
 from api.xsd_converter_api import create_app
 
-
-def main() -> NoReturn:
-    """Run the application using the application factory pattern."""
-    app = create_app(index_html="index.html")
-    app.run(debug=True)
-
+app = create_app(index_html="index.html")
 
 if __name__ == "__main__":
-    main()
+    app.run()
